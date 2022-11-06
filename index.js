5cf2e8d1ac5269275aab5b90d6eaac008be7dad7
SoutouKakka <chengyuikiu@gmail.com>
2022-11-06 14:56:19 +0900

    Update bot package

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-'use strict';
+"use strict";
 
-const bot = require('./lib/telegram/bot/index').getBot();
-const messageHandler = require('./lib/telegram/message_handler');
-
-bot.start();
+const bot = require("./lib/telegram/bot/index").getBot();
+const messageHandler = require("./lib/telegram/message_handler");
 
 /**
  * Commands
  */
 // Direct command query
-bot.on(/^\/cb_([1-5])(r(4|11|31)|:(4|11|31))?$/i, messageHandler.command.getClanBattleByCommand);
+bot.on(
+  /^\/cb_([1-5])(r(4|11|31)|:(4|11|31))?$/i,
+  messageHandler.command.getClanBattleByCommand
+);
 
 // Set chat default cycle
 bot.on(/^\/cbd_(4|11|31)/, messageHandler.command.updateClanBattleDefault);
 
 // Set chat custom sticker
-bot.on(/^\/cbs_([1-5]|teams)$/i, messageHandler.command.updateClanBattleSticker);
+bot.on(
+  /^\/cbs_([1-5]|teams)$/i,
+  messageHandler.command.updateClanBattleSticker
+);
 
 // Unset chat custom sticker
 bot.on(/^\/cbs_remove$/i, messageHandler.command.removeClanBattleSticker);
@@ -23,22 +27,5 @@ bot.on(/^\/cbs_remove$/i, messageHandler.command.removeClanBattleSticker);
 // Recommended teams
 bot.on(/^\/cb_teams/i, messageHandler.command.getClanTeamRecommendation);
 
-
-
-/**
- * Others
- */
-// Handler for inline mode
-bot.on('inlineQuery', messageHandler.inlineQuery);
-
 // Handler for all stickers
-bot.on('sticker', messageHandler.sticker);
-
-/**
- * Errors
- */
-bot.on('error', async (err) => {
-	const {data: msg} = err;
-	if (!msg) return;
-	await msg.reply.text('出事了');
-});
+bot.on("sticker", messageHandler.sticker);
